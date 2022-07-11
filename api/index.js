6567927a4f894a68994a8e82e1e6078da06ac84e
Sunil Buddala <53546018+sunil-buddala@users.noreply.github.com>
2022-07-11 17:15:37 +1200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World from API!')
+  res.send('Hello World from ' + process.env.ASPNETCORE_ENVIRONMENT)
 })
 
 app.listen(5000, () => console.log('Server is up and running'));
